2e7f2b5d571f416847a218e2e840118bda66d286
nikhil-techversant <nikhil.m@techversantinfo.com>
2022-09-14 13:21:46 +0530

    Update callbackToPromise.js

--- NodeJS_Exercises/callbackToPromise.js
+++ NodeJS_Exercises/callbackToPromise.js
@@ -1,57 +1,46 @@
+const { response } = require('express');
+
 const prompt = require('prompt-sync')();
 
 let num1 = Number(prompt("enter the 1st number : "));
 let num2 = Number(prompt("enter the 2nd number : "));
 
-function final(num1, num2) {
+function sum(result1) {
     return new Promise((resolve, reject) => {
-        result1 = sum(num1, num2)
-        result2 = mult(num1, num2)
-        result3 = div(num1, num2)
-        result4 = sub(num1, num2)
-        finalResult = result1 + result2 + result3 + result4;
-        resolve()
+        result1 = num1 + num2
+        resolve(result1)
+    })
+
+}
+function mult(result2) {
+    return new Promise((resolve, reject) => {
+        result2 = num1 * num2
+        resolve(result2)
+    })
+
+}
+function div(result3) {
+    return new Promise((resolve, reject) => {
+        result3 = num1 / num2
+        resolve(result3)
+    })
+
+}
+function sub(result4) {
+    return new Promise((resolve, reject) => {
+        result4 = num1 - num2
+        resolve(result4)
+
     })
 }
 
-const sum=(num1, num2)=> {
-    result1 = num1 + num2
-    return new Promise((resolve, reject) => {
-        resolve()
-    }).then(()=>{
-        const mult = (num1, num2)=> {
-            result2 = num1 * num2
-            return result2
-        }
-
+sum().then((result1) => {
+    return mult().then((result2) => {
+        return div().then((result3) => {
+            return sub().then((result4) => {
+                finalResult = result1 + result2 + result3 + result4
+                console.log(`Final Result  = ${finalResult}`)
+            })
+        })
     })
-}
-
-
-
-
-
-
-
-const p = final(num1, num2);
-
-p.then(() => {
-    console.log("Final Result : ", finalResult);
-}).catch(() => {
-    console.error("error");
 })
-
-
-
-
-
-function div(num1, num2) {
-    result3 = num1 / num2
-    return result3
-}
-function sub(num1, num2) {
-    result4 = num1 - num2
-    return result4
-}
-
-
