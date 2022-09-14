b83068f39196f49893e263910f3463c0d7592af5
nikhil-techversant <nikhil.m@techversantinfo.com>
2022-09-14 11:36:46 +0530

    Update callbacksample.js

--- NodeJS_Exercises/callbacksample.js
+++ NodeJS_Exercises/callbacksample.js
@@ -2,30 +2,30 @@ const prompt = require('prompt-sync')();
 
 let num1 = Number(prompt("enter the 1st number : "));
 let num2 = Number(prompt("enter the 2nd number : "));
-final(num1,num2)
 
-function sum(num1,num2){
-    result1 = num1+num2
-    return result1
+function sum(result1) {
+    result1(num1 + num2)
 }
-function mult(num1,num2){
-    result2 = num1*num2
-    return result2
+function mult(result2) {
+    result2(num1 * num2)
 }
-function div(num1,num2){
-    result3 = num1/num2
-    return result3
+function div(result3) {
+    result3(num1 / num2)
 }
-function sub(num1,num2){
-    result4 = num1-num2
-    return result4
+function sub(result4) {
+    result4(num1 - num2)
 }
-function final(num1,num2){
-    result1 = sum(num1,num2)
-    result2 = mult(num1,num2)
-    result3 = div(num1,num2)
-    result4 = sub(num1,num2)
-    finalResult = result1 +result2+result3+result4;
-    console.log("Final Result : ",finalResult);
-}
+
+sum((result1) => {
+    mult((result2) => {
+        div((result3) => {
+            sub((result4) => {
+                finalResult = result1 + result2 + result3 + result4
+                console.log(`Final Result = ${finalResult}`)
+            })
+        })
+
+    })
+})
+
 
